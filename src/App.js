66fc5eb9330b4e8611a2cd112da768c8f6be294a
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-07 22:47:12 -0500

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import GlobalStyle from './globalStyles';
+import { Routes, Router } from 'react-router-dom';
+import Home from './pages/Home';
+import Membership from './pages/Membership';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+
+      <GlobalStyle>
+        
+      </GlobalStyle>
+      <Home />
+      <Membership />
+      <Contact />
+      </Routes>
     </div>
   );
 }
