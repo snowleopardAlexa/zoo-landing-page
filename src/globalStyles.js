ecae810eef2c20e08e13b76d33212bd4a82954d2
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-07 22:37:30 -0500

    add global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,46 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle `
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Mukta', sans-serif;
+}
+`;
+
+export const Container = styled.div `
+
+`;
+
+export const MainHeading = styled.h1 `
+
+`;
+
+export const Heading = styled.div `
+
+`;
+
+export const TextWrapper = styled.h1 `
+
+`;
+
+export const Section = styled.div `
+
+`;
+
+export const Row = styled.h1 `
+
+`;
+
+export const Column = styled.div `
+
+`;
+
+export const Button = styled.h1 `
+
+`;
+
+export default GlobalStyle;
+
+
