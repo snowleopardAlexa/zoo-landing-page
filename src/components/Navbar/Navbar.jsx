880af09e18fcb1e2962f1dc87384768700fb5f93
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-09 11:39:48 -0500

    add jsx markup nav

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { FaRProject, FaTimes } from 'react-icons/fa';
+import { CgMenuRight } from 'react-icons/cg';
+import { IconContext } from 'react-icons';
+import {
+	Nav,
+	NavbarContainer,
+	NavLogo,
+	NavIcon,
+	MobileIcon,
+	NavMenu,
+	NavLinks,
+	NavItem,
+} from './NavbarStyles.js';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { data } from '../../data/NavbarData';
+
+const Navbar = () => {
+    return (
+     <Nav>
+         <NavbarContainer>
+             <NavLogo to="/">
+                 <NavIcon src="./assets/logo.png" alt="logo" />
+                 Panaewa Zoo
+             </NavLogo>
+             <MobileIcon>
+
+             </MobileIcon>
+             <NavMenu>
+                 <NavItem>
+                     <NavLinks></NavLinks>
+                 </NavItem>
+             </NavMenu>
+         </NavbarContainer>
+     </Nav>
+    );
+}
