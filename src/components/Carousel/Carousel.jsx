5049b2dbb6f812c75272262fd155a637a855628a
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-14 15:28:54 -0500

    add styles

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import { data, sliderSettings } from '../../data/CarouselData';
+import { Row, Heading, Section, TextWrapper } from '../../globalStyles';
+import {
+	ButtonContainer,
+	ReviewSlider,
+	ImageWrapper,
+	CarouselImage,
+	CardButton,
+} from './CarouselStyles';
+
+const Carousel = () => {
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel;
---
+++ src/components/Carousel/CarouselStyles.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Row } from '../../globalStyles';
+import Slider from 'react-slick';
+
+
+export const ButtonContainer = styled.div `
+
+`;
+
+export const ReviewSlider = styled.div `
+
+`;
+
+export const ImageWrapper = styled.div `
+
+`;
+
+export const CarouselImage = styled.div `
+
+`;
+
+export const CardButton = styled.div `
+
+`;
